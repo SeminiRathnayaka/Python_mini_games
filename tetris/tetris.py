2681e5b13e670a55355ec79a4c97d9c520156caa
Semini Rathnayaka <seminirathnayaka.yt@gmail.com>
2026-05-22 17:31:28 +0530

    Update tetris game logic

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -3,7 +3,7 @@ import sys
 from grid import Grid
 
 pygame.init()
-dark_blue = (0,0,128)
+dark_blue = (44,44,128)
 
 
 screen = pygame.display.set_mode((300,600))
@@ -11,6 +11,14 @@ pygame.display.set_caption("Tetris")
 grid = Grid()
 
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
+
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get():
@@ -21,6 +29,7 @@ while True:
 
     #Drawing the background
     screen.fill(dark_blue)
+    game_grid.draw(screen)
   
     pygame.display.update()
     clock.tick(60)        
